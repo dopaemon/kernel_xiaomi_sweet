fed536278efd6a9161036d2c30029c697682b302
dopaemon <polarisdp@gmail.com>
2023-05-25 21:08:02 +0700

    [INITIAL DORAEMON KERNEL]
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- scripts/DoraCore/build.sh
+++ scripts/DoraCore/build.sh
@@ -1,21 +1,56 @@
 #!/bin/bash
 
-#set -e
+# set -e
+
+# Reset commit
+curl -s "https://api.github.com/repos/dopaemon/kernel_xiaomi_sweet" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -o /tmp/repo_info.json
+default_branch=$(jq -r '.default_branch' /tmp/repo_info.json)
+curl -s "https://api.github.com/repos/dopaemon/kernel_xiaomi_sweet/commits/$default_branch" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -o /tmp/commit_info.json
+latest_commit_sha=$(jq -r '.sha' /tmp/commit_info.json)
+
+echo "Latest commit SHA: $latest_commit_sha"
+git reset --hard $latest_commit_sha
+
+if [ "$1" = "OSS" ]; then
+    git config --local user.name "dopaemon"
+    git config --local user.email "polarisdp@gmail.com"
+    git apply $PWD/scripts/github/ln8k.patch
+elif [ "$1" = "OSS-LN8000" ]; then
+    echo "oss"
+elif [ "$1" = "MIUI" ]; then
+    git config --local user.name "dopaemon"
+    git config --local user.email "polarisdp@gmail.com"
+    git apply $PWD/scripts/github/ln8k.patch
+    git apply $PWD/scripts/github/miui.patch
+elif [ "$1" = "MIUI-LN8000" ]; then
+    git config --local user.name "dopaemon"
+    git config --local user.email "polarisdp@gmail.com"
+    git apply $PWD/scripts/github/miui.patch
+else
+    echo "OUT"
+fi
+
+## Kernel Version
+linuxversion=$(make kernelversion)
+
+sed -i "s/vLINUX_VERSION/$linuxversion/g" $PWD/scripts/Anykernel3/banner
 
 ## Copy this script inside the kernel directory
-KERNEL_DEFCONFIG=sweet_defconfig ## Ini defconfignya setiap type hape beda2 (redmi note 10 pro menggunakan sweet_defconfig)
-ANYKERNEL3_DIR=$PWD/AnyKernel3/ ## ini anykernel nya gunanya untuk membukus hasil compile untuk siap flash
-FINAL_KERNEL_ZIP=Aghisna-Kernel-$(date '+%Y%m%d').zip ## INI NAMA KERNEL zip NYA
+KERNEL_DEFCONFIG=sweet-perf_defconfig
+ANYKERNEL3_DIR=$PWD/scripts/Anykernel3/
+FINAL_KERNEL_ZIP=DoraCore-Kernel-$1-$(date '+%Y%m%d').zip
 export PATH="$HOME/cosmic/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_COMPILER_STRING="$($HOME/cosmic/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-git clone --depth=1 -b master https://github.com/RooGhz720/Anykernel3.git AnyKernel3
-
 if ! [ -d "$HOME/cosmic" ]; then
 echo "Cosmic clang not found! Cloning..."
-if ! git clone -q https://gitlab.com/PixelOS-Devices/playgroundtc.git --depth=1 -b 17 ~/cosmic; then ## ini Clang nya tools untuk membangun/compile kernel nya (tidak semua kernel mendukung clang)
+if ! git clone -q https://gitlab.com/PixelOS-Devices/playgroundtc.git --depth=1 -b 17 ~/cosmic; then
 echo "Cloning failed! Aborting..."
 exit 1
 fi
@@ -30,11 +65,6 @@ cyan='\033[0;36m'
 yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
-
-# Clean build always lol
-echo "**** Cleaning ****"
-mkdir -p out
-make O=out clean
 
 echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
 echo -e "$blue***********************************************"
@@ -61,13 +91,17 @@ ls $PWD/out/arch/arm64/boot/dtbo.img
 ls $PWD/out/arch/arm64/boot/dtb.img
 
 # Anykernel 3 time!!
-echo "**** Verifying AnyKernel3 Directory ****"
-ls $ANYKERNEL3_DIR
-echo "**** Removing leftovers ****"
-rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
-rm -rf $ANYKERNEL3_DIR/dtbo.img
-rm -rf $ANYKERNEL3_DIR/dtb.img
-rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+# echo "**** Verifying AnyKernel3 Directory ****"
+# ls $ANYKERNEL3_DIR
+# echo "**** Removing leftovers ****"
+# rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+# rm -rf $ANYKERNEL3_DIR/dtbo.img
+# rm -rf $ANYKERNEL3_DIR/dtb.img
+# rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+
+rm -rf $ANYKERNEL3_DIR/*
+
+git checkout $latest_commit_sha $ANYKERNEL3_DIR/
 
 echo "**** Copying Image.gz-dtb & dtbo.img ****"
 cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
@@ -75,27 +109,20 @@ cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
 cp $PWD/out/arch/arm64/boot/dtb.img $ANYKERNEL3_DIR/
 
 echo "**** Time to zip up! ****"
+mkdir -p ZIPOUT
 cd $ANYKERNEL3_DIR/
 zip -r9 "../$FINAL_KERNEL_ZIP" * -x README $FINAL_KERNEL_ZIP
-
+mv -v * ../../ZIPOUT/
 echo "**** Done, here is your sha1 ****"
-cd ..
-rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
-rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
-rm -rf $ANYKERNEL3_DIR/dtbo.img
-rm -rf $ANYKERNEL3_DIR/dtb.img
-rm -rf out/
+cd ../../
+# rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+# rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+# rm -rf $ANYKERNEL3_DIR/dtbo.img
+# rm -rf $ANYKERNEL3_DIR/dtb.img
+# rm -rf out/
 
-sha1sum $FINAL_KERNEL_ZIP
+# sha1sum $FINAL_KERNEL_ZIP
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
-
-echo "**** Uploading your zip now ****"
-if command -v curl &> /dev/null; then
-curl -T $FINAL_KERNEL_ZIP temp.sh
-else
-echo "Zip: $FINAL_KERNEL_ZIP"
-fi
-
